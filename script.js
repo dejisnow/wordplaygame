85ff2eaa9bf2e5c885c2748c8cb0659f7c7180eb
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-08-23 23:32:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,23 +7,33 @@ window.onload=()=>{
    
    ];
    let password = document.querySelector(".password")
-   
+   let inputWord = document.querySelector(".inputWord");
  
  let checkBtn = document.querySelector(".check"); 
    let text = document.querySelector(".text"); 
    
-   let random = Math.floor(Math.random() * 10);
-  let wordPick = words[random];
+let random = Math.floor(Math.random()* 10)+1;
+ let wordPick = words[random];
   
   
-  let splitWord = wordPick.toString().split("");
+  let splitWord = wordPick;
+ console.log(typeof(wordPick))
+ let lowerCase = wordPick.toLowerCase()
  
- 
-  for(let j =1; j < splitWord.length-2; j++){
+  for(let j =0; j < splitWord.length-2; j++){
   
   text.innerHTML += `<span class="key"> ${splitWord[j]}</span>`
      
      
+     checkBtn.addEventListener("click",()=>{
+     let inputVal = inputWord.value
+     let inputLowerCase = inputVal.toLowerCase()
+         if(inputLowerCase === lowerCase ){
+             console.log("You got it !!!")
+         }else{
+             console.log("Wrong word")
+         }
+     })
          
      
     }
@@ -31,4 +41,4 @@ window.onload=()=>{
     
     
     
-  }
+                 }
